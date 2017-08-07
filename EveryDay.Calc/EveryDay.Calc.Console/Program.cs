7c1162074d4758ddbf4298dc6e8f770ada6cd59a
fantaziazashkalivaet <fantaziazashkalivaet@gmail.com>
2017-08-07 13:00:16 +0400

    Calc

--- EveryDay.Calc/EveryDay.Calc.Console/Program.cs
+++ EveryDay.Calc/EveryDay.Calc.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using SConsole = System.Console;
+﻿using EveryDay.Calc.Calculation;
+using SConsole = System.Console;
 
 namespace EveryDay.Calc.Console
 {
@@ -6,8 +7,43 @@ namespace EveryDay.Calc.Console
     {
         static void Main(string[] args)
         {
-            SConsole.WriteLine("test");
+            //SConsole.WriteLine("test");
+
+            var oper = args[0];
+            var x = Str2Int(args[1]);
+            var y = Str2Int(args[2]);
+
+            var calc = new Calculator();
+            int result = 0;
+
+            if (oper == "sum")
+            {
+                result = calc.Sum(x, y);
+            }
+            else if (oper == "div")
+            {
+                result = calc.Div(x, y);
+            }
+            else
+            {
+                SConsole.WriteLine("Нет такой операции");
+            }
+
+            SConsole.WriteLine(result.ToString());
+
             SConsole.ReadKey();
+        }
+
+        private static int Str2Int(string str)
+        {
+            var x = int.Parse(str);
+            int y;
+            if (!int.TryParse(str, out y))
+            {
+                SConsole.WriteLine("Не удалось преобразовать \"{0}\" в число", str);
+            }
+
+            return y;
         }
     }
 }
